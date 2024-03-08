97a82f6acded956fdff04099a24766c79b4c6db5
Elaidzha1940 <eli080293@gmail.com>
2024-03-08 17:22:56 +0300

    Update BankCard

--- BankCards/AppDelegate.swift
+++ BankCards/AppDelegate.swift
@@ -1,16 +1,16 @@
+//  /*
 //
-//  AppDelegate.swift
-//  BankCards
+//  Project: BankCards
+//  File: AppDelegate.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.03.2024
 //
-//  Created by Elaidzha Shchukin on 08.03.2024.
-//
+//  */
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,7 +30,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
--- BankCards/ViewController.swift
+++ BankCards/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "background")
